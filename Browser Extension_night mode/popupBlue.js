d102e21207e938800a625c2115967891fcba3eea
zhrxxxx <77419993+zhrxxxx@users.noreply.github.com>
2022-01-26 14:26:24 -0500

    Add files via upload

---
+++ Browser Extension_night mode/popupBlue.js
@@ -0,0 +1,19 @@
+﻿chrome.storage.sync.get("colorBlue", ({ colorBlue }) => {
+    changeColorBlue.style.backgroundColor = colorBlue;
+});
+
+changeColorBlue.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColorBlue,
+    });
+});
+
+
+function setPageBackgroundColorBlue() {
+    chrome.storage.sync.get("colorBlue", ({ colorBlue }) => {
+        document.body.style.backgroundColor = colorBlue;
+    });
+}
---
+++ Browser Extension_night mode/popupPurple.js
@@ -0,0 +1,19 @@
+﻿chrome.storage.sync.get("colorPurple", ({ colorPurple }) => {
+    changeColorPurple.style.backgroundColor = colorPurple;
+});
+
+changeColorPurple.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColorPurple,
+    });
+});
+
+
+function setPageBackgroundColorPurple() {
+    chrome.storage.sync.get("colorPurple", ({ colorPurple }) => {
+        document.body.style.backgroundColor = colorPurple;
+    });
+}
